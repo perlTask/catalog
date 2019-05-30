46795104e8bd08b8f492d7b04756c5da936e61cc
DmitryChernyavskiy <chernyavskydmitry@gmail.com>
2019-05-30 18:52:06 +0300

    statements

---
+++ App/statements.pm
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+package App::statements;
+
+use strict;
+use JSON;
+use Data::Dumper;
+
+sub new
+{
+    my $class = ref($_[0]) || $_[0];
+    my $self={};
+    my $data;
+
+    return bless($self, $class);
+}
+sub setData
+{
+  #1. json
+  my ($self) = $_[0];
+  $self->{'data'} = encode_json($_[1]);
+
+  #2. ref
+  #my ($self) = $_[0];
+  #$self->{'data'} = $_[1];
+  printf "reftype 2:%s\n", $self->{'data'};
+}
+
+sub getData
+{
+  #1. json
+  my ($self) = @_;
+  return decode_json($self->{'data'});
+
+  #2. ref
+  #my ($self) = @_;
+  #return $self->{'data'};
+}
+
+1;
